001f39f535ac2c8638cbc8aef61e48172f4fb125
ies-project <105677532+ies-project@users.noreply.github.com>
2022-05-18 02:28:09 +0100

    delete draft ICT.PL.Web

--- implementation/ICT/ICT.PL.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ICT.PL.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
