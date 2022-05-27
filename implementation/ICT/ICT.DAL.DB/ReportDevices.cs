19563decc690b7fbbd84b48bbf16477c5e2b9be1
Tiago Fernandes <aluno22102@ipt.pt>
2022-05-27 12:34:01 +0100

    Criação das classes

---
+++ implementation/ICT/ICT.DAL.DB/ReportDevices.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICT.DAL.DB
+{
+    internal class ReportDevices
+    {
+        public int Id_Report { get; set; }
+        public int Id_Device { get; set; }
+        public int Id_Area { get; set; }
+        public string State { get; set; }
+    }
+}
+
+
+
+
