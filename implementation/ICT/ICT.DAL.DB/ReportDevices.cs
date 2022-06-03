29ee8eeb52d85155769ccd08744d3e9a8d4e4a85
Tiago Fernandes <aluno22102@ipt.pt>
2022-06-03 11:06:00 +0100

    Finalização da criação da tabela ReportDevices

--- implementation/ICT/ICT.DAL.DB/ReportDevices.cs
+++ implementation/ICT/ICT.DAL.DB/ReportDevices.cs
@@ -1,17 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ICT.DAL.DB
 {
-    internal class ReportDevices
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ReportDevices
     {
         public int Id_Report { get; set; }
         public int Id_Device { get; set; }
+
         public int Id_Area { get; set; }
+
+        [StringLength(1, ErrorMessage = "The {0} cannot have more than 30 characters")]
         public string State { get; set; }
+
+        //public Devices Device { get; set; }
+
+        //public Reports Report { get; set; }
+ 
     }
 }
 
