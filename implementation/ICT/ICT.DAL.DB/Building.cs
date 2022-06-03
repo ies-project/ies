c856d0eaaf1ae2070e6660b0687808c4de45fdd6
anagracio <anitagracio@hotmail.com>
2022-06-03 11:47:48 +0100

    Corrected Building class

--- implementation/ICT/ICT.DAL.DB/Building.cs
+++ implementation/ICT/ICT.DAL.DB/Building.cs
@@ -1,8 +1,33 @@
 ﻿using System;
-
-public class Class1
+namespace ICT.DAL.DB
 {
-	public Class1()
+	/// <summary>
+	/// Building class
+	/// </summary>
+	public class Building
 	{
+		/// <summary>
+		/// Primary key of Area
+		/// </summary>
+		[Key]
+		public int Id { get; set; }
+		/// <summary>
+		/// Name of the Building
+		/// </summary>
+		[StringLength(32)]
+		public string Name { get; set; }
+		/// <summary>
+		/// Address of the Building
+		/// </summary>
+		[StringLength(32)]
+		public string Address { get; set; }
+		/// <summary>
+		/// Type of Building
+		/// </summary>
+		[StringLength(32)]
+		public string Type { get; set; }
+		/// <summary>
+		/// Collection of the Areas with this Building
+		/// </summary>
+		public ICollection<Area> Areas { get; set; }
 	}
-}
