274a22edab272a4d3e384df4b58c6feb864e8117
Tiago Fernandes <aluno22102@ipt.pt>
2022-06-06 18:12:00 +0100

    IES-26-create-device-requested-read-class

--- implementation/ICT/ICT.DAL.DB/Models/DeviceRequestedRead.cs
+++ implementation/ICT/ICT.DAL.DB/Models/DeviceRequestedRead.cs
@@ -14,34 +14,34 @@ namespace ICT.DAL.DB
     {
 
         /// <summary>
-        /// 
+        /// Primary key of DeviceRequestedReads
         /// </summary>
         [Key]
         public int Id { get; set; }
 
         /// <summary>
-        /// 
+        /// Foreign Key from table Device
         /// </summary>
         public int Id_Device { get; set; }
 
         /// <summary>
-        /// 
+        /// Date of the request
         /// </summary>
         public DateTime RequestDate { get; set; }
 
         /// <summary>
-        /// 
+        /// Date of the response
         /// </summary>
         public DateTime ResponseDate { get; set; }
 
         /// <summary>
-        /// 
+        /// Status of the response
         /// </summary>
         [StringLength(64, ErrorMessage = "The {0} cannot have more than 64 characters")]
         public string ResponseStatus { get; set; }
         
         /// <summary>
-        /// 
+        /// Body of the response
         /// </summary>
         [StringLength(500, ErrorMessage = "The {0} cannot have more than 500 characters")]
         public string ResponseBody { get; set; }
--- implementation/ICT/ICT.DAL.DB/Models/ReportDevice.cs
+++ implementation/ICT/ICT.DAL.DB/Models/ReportDevice.cs
@@ -8,29 +8,29 @@ using System.Threading.Tasks;
 namespace ICT.DAL.DB
 {
     /// <summary>
-    /// Data of devices that can be report
+    /// Data of devices that can be reported
     /// </summary>
-    public class ReportDevices
+    public class ReportDevice
     {
         /// <summary>
-        /// 
+        /// Primary key of ReportDevice
         /// </summary>
         [Key]
         public int Id_Report { get; set; }
 
         /// <summary>
-        /// 
+        /// Primary key of ReportDevice
         /// </summary>
         [Key]
         public int Id_Device { get; set; }
 
         /// <summary>
-        /// 
+        /// Foreign Key from table Area
         /// </summary>
         public int Id_Area { get; set; }
 
         /// <summary>
-        /// 
+        /// State of a Device
         /// </summary>
         [StringLength(32, ErrorMessage = "The {0} cannot have more than 32 characters")]
         public string State { get; set; }
