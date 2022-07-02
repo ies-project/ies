72dc745f2545ef75332aba03c9cc198a05878e6d
Tiago Fernandes <aluno22102@ipt.pt>
2022-07-02 16:35:58 +0100

    Create SurroundingAreaBLL its controller and DTO's

---
+++ implementation/ICT/ICT.BLL/SurroundingAreaBLL.cs
@@ -0,0 +1,70 @@
+﻿using ICT.Core.DTO;
+using ICT.DAL.DB;
+
+public class SurroundingAreaBLL
+{
+
+    public static void InsertSurroundingArea(InsertSurroundingAreaRequestDTO dto)
+    {
+        using (ICTDbContext db = new ICTDbContext())
+        {
+
+            SurroundingArea newSurroundingArea = new SurroundingArea();
+
+            newSurroundingArea.Id = dto.Id;
+
+            newSurroundingArea.Id_Area = dto.Id_Area;
+
+            db.SurroundingAreas.Add(newSurroundingArea);
+
+            db.SaveChanges();
+        }
+    }
+
+    public static void DeleteSurroundingArea(DeleteSurroundingAreaRequestDTO dto)
+    {
+        using (ICTDbContext db = new ICTDbContext())
+        {
+
+            db.SurroundingAreas.Remove(db.SurroundingAreas.Find(dto.Id));
+
+            db.SurroundingAreas.RemoveRange(db.SurroundingAreas.Where(x => x.Id == dto.Id));
+
+            db.SaveChanges();
+
+        }
+    }
+
+    public static void UpdateSurroundingArea(UpdateSurroundingAreaRequestDTO dto)
+    {
+        using (ICTDbContext db = new ICTDbContext())
+        {
+            SurroundingArea newSurroundingArea = db.SurroundingAreas.Find(dto.Id);
+
+            newSurroundingArea.Id_Area = dto.Id_Area;
+
+            db.SurroundingAreas.Add(newSurroundingArea);
+
+            db.SaveChanges();
+        }
+    }
+
+    public static ListSurroundingAreaResponseDTO ListSurroundingArea()
+    {
+        using (ICTDbContext db = new ICTDbContext())
+        {
+            List<ListItemSurroundingAreaResponseDTO> listItemSurroundingAreaResponseDTOs = db.SurroundingAreas
+                    .Select(x => new ListItemSurroundingAreaResponseDTO
+                    {
+                        Id = x.Id,
+                        Id_Area = x.Id_Area,               
+                    }).ToList();
+
+            return new ListSurroundingAreaResponseDTO
+            {
+                Items = listItemSurroundingAreaResponseDTOs,
+                Total = listItemSurroundingAreaResponseDTOs.Count()
+            };
+        }
+    }
+}
---
+++ implementation/ICT/ICT.Core/DTO/UpdateSurroundingAreaRequestDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICT.Core.DTO {
+    public class UpdateSurroundingAreaRequestDTO {
+        public int Id { get; set; }
+        public int Id_Area { get; set; }
+    }
+}
