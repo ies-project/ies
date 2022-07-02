8f20b74795358aa248dfcb2a2d4a11761c2316aa
vasco2001 <102040726+vasco2001@users.noreply.github.com>
2022-07-02 10:31:41 +0100

    Correção dos ScenariosBLL

---
+++ implementation/ICT/ICT.MM.BLL/ScenariosBLL.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ICT.MM.Core.DTO;
+using ICT.MM.DAL.DB;
+
+namespace ICT.MM.BLL {
+    public class ScenariosBLL {
+
+        public static void InsertScenario(int id, string name, string description)
+        {
+            using (ICT.MM.DAL.DB.ICTDbContext iCTDbContext = new ICT.MM.DAL.DB.ICTDbContext())
+            {
+                Scenario sc = new Scenario();
+                sc.Name = name;
+                sc.Description = description;
+                sc.Id = id;
+
+                iCTDbContext.Scenarios.Add(sc);
+
+                iCTDbContext.SaveChanges();
+            }
+        }
+        
+        public static void DeleteScenario(DeleteScenarioDTO dto)
+        {
+            using (ICTDbContext iCTDbContext = new ICTDbContext())
+            {
+                iCTDbContext.ScenarioDevices.RemoveRange(iCTDbContext.ScenarioDevices.Where(x => x.Id_Scenario == id));
+
+                iCTDbContext.Scenarios.Remove(iCTDbContext.Scenarios.Find(dto.Id));
+
+                iCTDbContext.SaveChanges();
+            }
+        }
+
+        public static void UpdateScenario(UpdateScenarioRequestDTO dto)
+        {
+            using (ICTDbContext iCTDbContext = new ICTDbContext())
+            {
+                Scenario sc = iCTDbContext.Scenarios.Find(dto.Id);
+
+                sc.Name = dto.Name;
+
+                sc.Description = dto.Descripton;
+
+                iCTDbContext.SaveChanges();
+            }
+        }
+
+        public static ListScenarioResponseDTO ListScenarios() {
+
+            using (ICTDbContext iCTDbContext = new ICTDbContext()){ 
+
+            List<ListItemScenarioResponseDTO> listItemScenarioResponseDTOs = iCTDbContext.Scenarios
+                    .Select(x => new ListItemScenarioResponseDTO { Id = x.Id, Name = x.Name}).ToList();
+
+                return new ListScenarioResponseDTO {Items = listItemScenarioResponseDTOs, Total = listItemScenarioResponseDTOs.Count()};
+            }
+        }
+
+    }
+}
---
+++ implementation/ICT/ICT.MM.PL.WebAPI/Controllers/ScenarioController.cs
@@ -0,0 +1,14 @@
+﻿using ICT.MM.Core.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ICT.MM.PL.WebAPI.Controllers {
+    [ApiController]
+    [Route("[controller]")]
+    public class ScenarioController : ControllerBase {
+        [HttpGet(Name = "GetScenarios")]
+        public ListScenarioResponseDTO Get()
+        {
+            return BLL.ScenariosBLL.ListScenarios();
+        }
+    }
+}
