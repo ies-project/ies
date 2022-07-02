69ad98dd1b01382a9a5e15d12e7c6e9a035feb9a
afsalmeida02 <afsalmeida02@gmail.com>
2022-07-02 15:05:30 +0100

    BLL Updated.

---
+++ implementation/ICT/ICT.MM.BLL/DeviceTypeBLL.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ICT.MM.Core.DTO;
+using ICT.MM.DAL.DB;
+
+namespace ICT.MM.BLL
+{
+    public class DeviceTypesBLL
+    {
+
+        public static void InsertDeviceType(InsertDeviceRequestDTO dto)
+        {
+            using (ICTDbContext iCTDbContext = new ICTDbContext())
+            {
+                if (iCTDbContext.DeviceTypes.Find(dto.Id) == null)
+                {
+                    DeviceType sc = new DeviceType();
+                    sc.Name = dto.Name;
+                    sc.Description = dto.Description;
+                    sc.Id = dto.Id;
+
+                    iCTDbContext.DeviceTypes.Add(sc);
+
+                    iCTDbContext.SaveChanges();
+                }
+            }
+        }
+
+        public static void DeleteDeviceType(DeleteDeviceRequestDTO dto)
+        {
+            using (ICTDbContext iCTDbContext = new ICTDbContext())
+            {
+                iCTDbContext.DeviceTypes.RemoveRange(iCTDbContext.DeviceTypes.Where(x => x.Id == dto.Id));
+
+                if (iCTDbContext.DeviceTypes.Find(dto.Id) != null)
+                {
+                    iCTDbContext.DeviceTypes.Remove(iCTDbContext.DeviceTypes.Find(dto.Id));
+
+                    iCTDbContext.SaveChanges();
+                }
+            }
+        }
+
+        public static void UpdateDeviceType(UpdateDeviceRequestDTO dto)
+        {
+            using (ICTDbContext iCTDbContext = new ICTDbContext())
+            {
+                DeviceType sc = iCTDbContext.DeviceTypes.Find(dto.Id);
+
+                sc.Name = dto.Name;
+
+                sc.Description = dto.Description;
+
+                iCTDbContext.SaveChanges();
+            }
+        }
+
+        public static ListDeviceResponseDTO ListDeviceTypes()
+        {
+
+            using (ICTDbContext iCTDbContext = new ICTDbContext())
+            {
+
+                List<ListItemDeviceResponseDTO> listItemDeviceTypeResponseDTOs = iCTDbContext.DeviceTypes
+                        .Select(x => new ListItemDeviceResponseDTO { Id = x.Id, Name = x.Name, Description = x.Description }).ToList();
+
+                return new ListDeviceResponseDTO { Items = listItemDeviceTypeResponseDTOs, Total = listItemDeviceTypeResponseDTOs.Count() };
+            }
+        }
+
+    }
+}
---
+++ implementation/ICT/ICT.MM.Core/DTO/ListDeviceResponseDTO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICT.MM.Core.DTO {
+    public class ListDeviceResponseDTO {
+        public int Total { get; set; }
+
+        public List<ListItemDeviceResponseDTO> Items { get; set; }
+    }
+
+    public class ListItemDeviceResponseDTO {
+        public int Id { get; set; }
+        public int Id_DeviceType { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime ManufacturedDate { get; set; }
+        public DateTime LastMaintenanceDate { get; set; }
+        public DateTime MaintenanceDueDate { get; set; }
+        public string ManufacturedBy { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
