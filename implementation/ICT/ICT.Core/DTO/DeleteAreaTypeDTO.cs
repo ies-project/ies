36194c8d964a2d0e654589959f94d5ea16fddd36
Rafael Pimentel <rafaelpimentel@outlook.pt>
2022-07-02 15:57:39 +0100

    ICT.BLL - Created AreaTypeBLL and respective DTOs

---
+++ implementation/ICT/ICT.Core/DTO/DeleteAreaTypeDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICT.Core.DTO
+{
+    public class DeleteAreaTypeDTO
+    {
+        public int Id { get; set; }
+    }
+}
