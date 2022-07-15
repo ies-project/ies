824e051f68de90dfdd6eaf651d23b6943abeeec0
vasco2001 <102040726+vasco2001@users.noreply.github.com>
2022-07-15 11:47:41 +0100

    Controller DeviceType MM

--- implementation/ICT/ICT.MM.Core/DTO/ListDeviceTypesResponseDTO.cs
+++ implementation/ICT/ICT.MM.Core/DTO/ListDeviceTypesResponseDTO.cs
@@ -14,13 +14,9 @@ namespace ICT.MM.Core.DTO
     }
     public class ListItemDeviceTypesResponseDTO
     {
-        public int Id_Scenario { get; set; }
-        public int Id_Device { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
-        public DateTime ManufacturedDate { get; set; }
-        public DateTime LastMaintenanceDate { get; set; }
-        public DateTime MaintenanceDueDate { get; set; }
-        public string OriginalState { get; set; }
-        public string CurrentState { get; set; }
+        public string Description { get; set; }
+        
     }
 }
---
+++ implementation/ICT/ICT.MM.PL.WebAPI/Controllers/DeviceTypeController.cs
@@ -0,0 +1,34 @@
+﻿using ICT.MM.BLL;
+using ICT.MM.Core.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ICT.MM.PL.WebAPI.Controllers {
+    [ApiController]
+    [Route("[controller]")]
+    public class DeviceTypeController : ControllerBase{
+
+        [HttpGet(Name = "GetDeviceTypes")]
+        public ListDeviceTypesResponseDTO GetList()
+        {
+            return DeviceTypesBLL.ListDeviceTypes();
+        }
+
+        [HttpDelete(Name = "DeleteDeviceType")]
+        public void DeleteDeviceType(DeleteDeviceRequestDTO dto)
+        {
+            DeviceTypesBLL.DeleteDeviceType(dto);
+        }
+
+        [HttpPut(Name = "InsertDeviceType")]
+        public void InsertDeviceType(InsertDeviceRequestDTO dto)
+        {
+            DeviceTypesBLL.InsertDeviceType(dto);
+        }
+
+        [HttpPatch(Name = "UpdateDeviceType")]
+        public void UpdateDevice(UpdateDeviceRequestDTO dto)
+        {
+            DeviceTypesBLL.UpdateDeviceType(dto);
+        }
+    }
+}
