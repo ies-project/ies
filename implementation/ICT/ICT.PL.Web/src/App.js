4f63f10365b927257a40eb7672af4b1c2a4a4344
Rafael Pimentel <rafaelpimentel@outlook.pt>
2022-07-17 00:07:45 +0100

    ICT.Pl.Web - React

--- implementation/ICT/ICT.PL.Web/src/App.js
+++ implementation/ICT/ICT.PL.Web/src/App.js
@@ -1,24 +1,21 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'jquery/dist/jquery.min.js'
+import 'bootstrap/dist/js/bootstrap.min.js'
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './layout/navbar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+          <div className='App'>
+              <Navbar />
+              <div className='content'>
+                 
+              </div>
+          </div>
+      </Router>
   );
 }
 
