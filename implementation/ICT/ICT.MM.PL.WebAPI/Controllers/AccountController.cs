ddb63dfd2c123be82dc2b978e7e5d396b113f13b
vasco2001 <vascoantonio.araujo@gmail.com>
2022-09-16 20:24:58 +0100

    [MM]Função de alterar palavra passe a funcionar

--- implementation/ICT/ICT.MM.PL.WebAPI/Controllers/AccountController.cs
+++ implementation/ICT/ICT.MM.PL.WebAPI/Controllers/AccountController.cs
@@ -139,28 +139,32 @@ namespace ICT.MM.PL.WebAPI.Controllers {
             return View(acc);
         }
         [HttpPost]
-        public async Task<IActionResult> Edit([Bind("Id,Username,Password,Name,Role")] Account acc)
+        [Authorize(Policy = "LoggedIn")]
+        public async Task<IActionResult> ChangePassword(string NewPass2,string NewPass1,string CurPass, [Bind("Id,Username,Password,Name,Role")] Account acc)
         {
-            var verifyAcc = db.Accounts.Where(a=> a.Id == acc.Id);
-            if (!(verifyAcc.Any(a => a.Username == acc.Username && a.Password == acc.Password) && HashPassword(ViewData["CurrentPass"].ToString()) == acc.Password))
+            var verifyAcc = await db.Accounts.AsNoTracking().Where(a => a.Id == acc.Id).FirstOrDefaultAsync();
+            if (verifyAcc == null)
+            { return NotFound(); }
+            Boolean bol = HashPassword(CurPass) == verifyAcc.Password;
+            if (!bol)
             {
                 //logout
                 await HttpContext.SignOutAsync(
                 CookieAuthenticationDefaults.AuthenticationScheme);
                 return RedirectToAction("Account", "Login");
             }
-            if(ViewData["NewPass1"] != ViewData["NewPass2"])
+            if(NewPass2 != NewPass1)
             {
                 ViewData["ErrorPassDiff"] = "As palavras passe não são iguais.";
                 return RedirectToAction("Account", "ChangePassword");
             }
 
-            if (StrongPassword(ViewData["NewPass1"].ToString()))
+            if (StrongPassword(NewPass1))
             {
-                acc.Password = HashPassword(ViewData["NewPass1"].ToString());
-                db.Accounts.Add(acc);
+                acc.Password = HashPassword(NewPass1);
+                db.Accounts.Update(acc);
                 db.SaveChanges();
-                return RedirectToAction("Home", "Index");
+                return RedirectToAction("Index", "Home");
             }
             else
             {
@@ -193,8 +197,8 @@ namespace ICT.MM.PL.WebAPI.Controllers {
                 return false;
             if(!password.Any(c => char.IsLower(c)))
                 return false;
-            if (!password.Any(c => char.IsSymbol(c)))
-                return false;
+            //if (!password.Any(c => char.IsSymbol(c)))
+            //    return false;
 
             return true;
         }
